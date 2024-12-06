221bb47530f3313102d29bf98b58d431dd426869
Joseph <jgoyer3@gmail.com>
2024-12-06 14:55:47 -0500

    day2

---
+++ day2-1.py
@@ -0,0 +1,41 @@
+def solve(input):
+    file = open(input)
+    content = file.readlines()
+    assert len(content) > 0
+    file.close()
+
+    count = 0
+
+    for report in content:
+        safe = True
+        s = 0
+
+        levels = report.split()
+        for n in range(len(levels) - 1):
+            a = int(levels[n])
+            b = int(levels[n + 1])
+
+            if a == b:
+                safe = False
+                break
+
+            change = a - b
+
+            if s == 0:
+                s = sign(change)
+            elif s != sign(change):
+                safe = False
+                break
+
+            if abs(change) > 3:
+                safe = False
+                break
+
+        if safe:
+            count += 1
+
+    return count
+
+
+def sign(x):
+    return (x > 0) - (x < 0)
---
+++ day2-2.py
@@ -0,0 +1,58 @@
+def solve(input):
+    file = open(input)
+    content = file.readlines()
+    assert len(content) > 0
+    file.close()
+
+    count = 0
+
+    for report in content:
+        levels = report.split()
+        if check(levels):
+            count += 1
+            continue
+
+        for n in range(len(levels)):
+            cut = report.split()
+            cut.pop(n)
+            if check(cut):
+                count += 1
+                break
+
+    return count
+
+
+def check(levels):
+    safe = True
+    s = 0
+
+    for n in range(len(levels) - 1):
+        a = int(levels[n])
+        b = int(levels[n + 1])
+
+        if is_safe(a, b, s):
+            if s == 0:
+                s = sign(a - b)
+        else:
+            safe = False
+
+    return safe
+
+
+def is_safe(a, b, s):
+    change = a - b
+
+    if change == 0:
+        return False
+
+    if s != 0 and s != sign(change):
+        return False
+
+    if abs(change) > 3:
+        return False
+
+    return True
+
+
+def sign(x):
+    return (x > 0) - (x < 0)
