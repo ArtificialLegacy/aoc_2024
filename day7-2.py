7e65bb2b36255b86eff8bf75b5a7757416228fc2
Joseph <jgoyer3@gmail.com>
2024-12-07 15:34:53 -0500

    day7

---
+++ day7-2.py
@@ -0,0 +1,43 @@
+def solve(input: str):
+    content = input.split("\n")
+
+    sum = 0
+
+    for test in content:
+        if test == "":
+            continue
+
+        d = test.split(":")
+        assert len(d) == 2
+
+        value = int(d[0])
+        nums = [int(x) for x in d[1].split()]
+
+        if step(value, nums[0], nums[1:], "+"):
+            sum += value
+        elif step(value, nums[0], nums[1:], "*"):
+            sum += value
+        elif step(value, nums[0], nums[1:], "||"):
+            sum += value
+
+    return sum
+
+
+def step(goal: int, current: int, nums: list[int], operator: str):
+    if operator == "+":
+        current += nums[0]
+    elif operator == "*":
+        current *= nums[0]
+    elif operator == "||":
+        current = int(str(current) + str(nums[0]))
+
+    if len(nums) == 1:
+        return current == goal
+    if current > goal:
+        return False
+
+    return (
+        step(goal, current, nums[1:], "+")
+        or step(goal, current, nums[1:], "*")
+        or step(goal, current, nums[1:], "||")
+    )
